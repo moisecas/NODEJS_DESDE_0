0a15ec4704dbf409756321fd1daea67994c959e9
Your Name <moisesjuniorcas20@gmail.com>
2022-04-22 14:46:15 -0500

    enviando y recibiendo respuestas por pantalla y en consola con id de conexión

--- models/server.js
+++ models/server.js
@@ -5,14 +5,14 @@ const cors = require('cors');
 
 
 
-class Server {
+class Server { //constructor del servidor 
 
     constructor() {
         this.app  = express();
         this.port = process.env.PORT;
         this.server = require('http').createServer(this.app); //importación de paquete, le paso mi servidor de express 
         this.io = require('socket.io')(this.server) //paquete de socket.io y le voy a mandar el this.server, información de los clientes conectados  
-
+        //io información de los sockets conectados  
         this.paths = {
           
         }
@@ -25,6 +25,12 @@ class Server {
 
         // Rutas de mi aplicación
         this.routes();
+
+
+        //Sockets, configuración, manejo de eventos en el socket 
+        this.sockets(); 
+
+
     }
 
 
@@ -50,11 +56,23 @@ class Server {
         
     }
 
+    sockets(){ //configuración de mis sockets 
+        this.io.on('connection', socket =>{ //hace referencia a nuestro servidor de sockets
+            console.log('cliente conectado', socket.id) //connection es un evento de sockets
+            
+            socket.on('disconnect', () =>{
+                console.log('el cliente ya no esta', socket.id) //socket.id identificador, le da un id a cada conexión
+            })
+        })
+    }
+
     listen() { //servidor oyendo peticiones 
         this.server.listen( this.port, () => { //levantando el server de la clase server 
             console.log('Servidor corriendo en puerto', this.port );
         });
     }
+
+
 
 }
 
--- public/js/socket-client.js
+++ public/js/socket-client.js
@@ -1,1 +1,23 @@
-alert("hola mundo") 
+//Referencias de html 
+
+const lblOnline = document.querySelector('#lblOnline'); 
+const lblOffline = document.querySelector('#lblOffline');  
+
+
+
+const socket = io();  //cliente, uso de objeto para exponer o ver los eventos del socket, io es lo que esta en el index html o a donde renderizemos nuestro index 
+
+//escuchando cambios o eventos 
+
+socket.on('connect', ()=>{ //saber cuando me conecto al servidor 
+    console.log('conectado')
+    lblOffline.style.display = 'none'
+    lblOnline.style.display = '' //para visualizar el estado por pantalla al cliente 
+}) //el on es para oír eventos 
+
+socket.on('disconnect', ()=>{ //saber cuando se desconecto el servidor
+    console.log('desconectado del server')
+    lblOnline.style.display = 'none' //para visualizar el estado por pantalla al cliente
+    lblOffline.style.display = '' 
+      
+}) 
